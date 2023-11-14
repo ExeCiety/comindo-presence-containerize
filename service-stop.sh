9b3696a4286bac7ccea0bb64a6cc7a1c45196ef4
Moch Sultan Majid <sultan.majid213@gmail.com>
2023-11-14 20:59:15 +0700

    initial-commit

---
+++ service-stop.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Set the directory where the Docker Compose YAML file is located
+COMPOSE_FILE=docker-compose.yml
+
+# Check if the Docker Compose file exists
+if [ ! -f $COMPOSE_FILE ]; then
+  echo "Docker Compose YAML file not found"
+  exit 1
+fi
+
+# Start the Docker Compose services
+docker compose -p presensi-comindo stop
